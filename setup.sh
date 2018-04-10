0c656d446e71d1b86340a0e4d7ba2b9120b447f4
jalmond <jalmond@cern.ch>
2018-04-11 00:33:50 +0900

    rename classes/files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # $Id: setup.sh 1 01/12/2013 jalmond $
 ###################################################################################
-# @Project: LQAnalyzer/SKTree - ROOT-based analysis framework for Korea CMS group #
+# @Project: Analyzer/SKTree - ROOT-based analysis framework for Korea CMS group #
 #                                                                                 #
 # @author John Almond       <jalmond@cern.ch>           - SNU                     #
 #                                                                                 #
@@ -15,10 +15,18 @@ echo "Setting up environment for compiling/running CATAnalzer with SKTree"
 
 setupok=False
 
-function killbkg {
-    python $LQANALYSER_DIR/python/killbkg.py -i $1
-}
+Flag=""
 
+if [[ $1 == "" ]]; then
+    Flag="SNU"
+
+else
+    Flag="LQ"
+fi
+
+function killbkg {
+    python $PWD/python/killbkg.py -i $1
+}
 
 while read line
   do
@@ -43,25 +51,25 @@ if [[ $USER == "jalmond" ]]; then
 	source python/jalmondsetup.sh
     fi
     function cat_path_analysis_ls {
-        ll -rth  /data2/CAT_SKTreeOutput/JobOutPut/jalmond/LQanalyzer/data/output/CAT/HNDiLepton/periodBtoH/${1}
+        ll -rth  /data2/CAT_SKTreeOutput/JobOutPut/jalmond/${Flag}analyzer/data/output/CAT/HNDiLepton/periodBtoH/${1}
     }
     function cat_path_analysis {
-	cd /data2/CAT_SKTreeOutput/JobOutPut/jalmond/LQanalyzer/data/output/CAT/HNDiLepton/periodBtoH/${1}
+	cd /data2/CAT_SKTreeOutput/JobOutPut/jalmond/${Flag}analyzer/data/output/CAT/HNDiLepton/periodBtoH/${1}
     }
 fi
 
 
 
 
-if [[ $PWD !=  *"/data4/LQAnalyzerCode/"* ]];
+if [[ $PWD !=  *"/data4/${Flag}AnalyzerCode/"* ]];
 then
     if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
     then
-        echo "Setup failed. LQanalyzer needs to be in /data4/LQAnalyzerCode/"$USER
-        if [ ! -d /data4/LQAnalyzerCode/$USER ]; then
-            mkdir /data4/LQAnalyzerCode/$USER
+        echo "Setup failed. ${Flag}analyzer needs to be in /data4/${Flag}AnalyzerCode/"$USER
+        if [ ! -d /data4/${Flag}AnalyzerCode/$USER ]; then
+            mkdir /data4/${Flag}AnalyzerCode/$USER
         fi
-        echo "Move the current LQAnalyzer directory to "/data4/LQAnalyzerCode/$USER
+        echo "Move the current ${Flag}Analyzer directory to "/data4/${Flag}AnalyzerCode/$USER
 
         return
     fi
@@ -69,7 +77,7 @@ fi
 
 
 if [ $LQANALYZER_DIR ]; then
-    echo LQANALYZER_DIR is already defined, use a clean shell
+    echo ${Flag}ANALYZER_DIR is already defined, use a clean shell
     return 1
 fi
 
@@ -89,8 +97,8 @@ else
 fi    
 
 
-# speficy the LQANALYZER_DIR base directory, i.e., the directory in which this file lives
-export LQANALYZER_DIR=${PWD}
+# speficy the ${Flag}ANALYZER_DIR base directory, i.e., the directory in which this file lives
+export ANALYZER_DIR=${PWD}
 
 
 
@@ -102,20 +110,20 @@ fi
 
 if [[ $1 == *"v7"* ]]; then
     echo "Setting up tag "$1
-    export CHECKTAGFILE=/data1/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
+    export CHECKTAGFILE=/data1/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
     if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
     then
-	export CHECKTAGFILE=/data2/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
+	export CHECKTAGFILE=/data2/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
     fi
 
     if [[ ! -f $CHECKTAGFILE ]]; then 
-	export LQANALYZER_DIR=""
+	export ANALYZER_DIR=""
 	echo $1 "is not allowed input. Use one of:"
 	
-	source /data1/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
+	source /data1/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
 	if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
 	then
-	    source /data2/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
+	    source /data2/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
 	fi
 	for ic in  ${list_of_catversions[@]};
         do
@@ -124,15 +132,15 @@ if [[ $1 == *"v7"* ]]; then
 	return 1
     
     fi
-    export LQANALYZER_MOD="/data1/LQAnalyzer_rootfiles_for_analysis/CATMOD2015/"
+    export ANALYZER_MOD="/data1/${Flag}Analyzer_rootfiles_for_analysis/CATMOD2015/"
     if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
         then
-	export LQANALYZER_MOD="/data2/LQAnalyzer_rootfiles_for_analysis/CATMOD2015/"
+	export ANALYZER_MOD="/data2/${Flag}Analyzer_rootfiles_for_analysis/CATMOD2015/"
 
     fi
-    source $LQANALYZER_DIR/bin/setup2015.sh
+    source $ANALYZER_DIR/bin/setup2015.sh
     export running2015=True
-    cvdir=$LQANALYZER_DIR/LQLib/$CATVERSION
+    cvdir=$ANALYZER_DIR/${Flag}Lib/$CATVERSION
     if [[ ! -d "${cvdir}" ]]; then
         mkdir $cvdir
         make distclean
@@ -141,13 +149,13 @@ if [[ $1 == *"v7"* ]]; then
     return 1
 fi
 
-export LQANALYZER_MOD="/data1/LQAnalyzer_rootfiles_for_analysis/CATMOD/"
+export ANALYZER_MOD="/data1/${Flag}Analyzer_rootfiles_for_analysis/CATMOD/"
 if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
 then
-    export LQANALYZER_MOD="/data2/LQAnalyzer_rootfiles_for_analysis/CATMOD/"
+    export ANALYZER_MOD="/data2/${Flag}Analyzer_rootfiles_for_analysis/CATMOD/"
 
 fi
-python ${LQANALYZER_DIR}/scripts/CheckEmailIsSetup.py
+python ${ANALYZER_DIR}/scripts/CheckEmailIsSetup.py
 cat_email="NULL"
 while read line
 do
@@ -157,24 +165,24 @@ do
         line=${line:${#prefix}}
         cat_email=$line
     fi
-done < ${LQANALYZER_DIR}/bin/catconfig
+done < ${ANALYZER_DIR}/bin/catconfig
 if [[ $cat_email  == "NULL" ]];
 then
     echo "Email not setup. run setup.sh again"
-    export LQANALYZER_DIR=""
+    export ANALYZER_DIR=""
     return 1
 fi
 
 ##### Check that this is not the branch and a tag was checked out
-export CHECKTAGFILE=$LQANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
+export CHECKTAGFILE=$ANALYZER_DIR/scripts/setup/SetBrachAndTag.sh
 source $CHECKTAGFILE branch
 
-source $LQANALYZER_DIR/bin/CheckTag.sh
+source $ANALYZER_DIR/bin/CheckTag.sh
 
-buglist=/data1/LQAnalyzer_rootfiles_for_analysis/CATTag/BuggyTag.txt
+buglist=/data1/${Flag}Analyzer_rootfiles_for_analysis/CATTag/BuggyTag.txt
 if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
 then
-    buglist=/data2/LQAnalyzer_rootfiles_for_analysis/CATTag/BuggyTag.txt
+    buglist=/data2/${Flag}Analyzer_rootfiles_for_analysis/CATTag/BuggyTag.txt
 fi
 
 while read line
@@ -189,20 +197,20 @@ done < $buglist
 export LIBTAG=""
 if [[ $1 != "" ]];then
 
-    export CHECKTAGFILE=/data1/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
+    export CHECKTAGFILE=/data1/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
     if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
     then
-	export CHECKTAGFILE=/data2/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
+	export CHECKTAGFILE=/data2/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/SetBrachAndTag_$1.sh
     fi
     if [[ ! -f $CHECKTAGFILE ]]; then
-	export LQANALYZER_DIR=""
+	export ANALYZER_DIR=""
         echo $1 "is not allowed input. Use one of:"
 	
 	if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
 	then
-            source /data2/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
+            source /data2/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
 	else
-	    source /data1/LQAnalyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
+	    source /data1/${Flag}Analyzer_rootfiles_for_analysis/CattupleConfig/$CATVERSION.sh
 
 	fi
         for ic in  ${list_of_catversions[@]};
@@ -226,120 +234,120 @@ alias sktree="bash submitSKTree.sh"
 alias sktreemaker="bash submitSKTree.sh -M True "
 alias sktree_val="bash submitSKTree.sh -V True "
 alias sktree_bkg="nohup bash submitSKTree.sh -b True "
-alias new_git_tag="bash "$LQANALYZER_DIR"/scripts/setup/git_newtag.sh"
+alias new_git_tag="bash "$ANALYZER_DIR"/scripts/setup/git_newtag.sh"
 alias git_commit_lq="bash scripts/setup/git_commit.sh"
 alias sktree_bkg_log="python python/PrintBkgJob.py"
 
-export LQANALYZER_FILE_DIR="/data1/LQAnalyzer_rootfiles_for_analysis/CATAnalysis2016/"
-export LQANALYZER_DATASETFILE_DIR="/data1/LQAnalyzer_rootfiles_for_analysis/DataSetLists/AnalysisFiles/"
-export LQANALYZER_DATASET_DIR="/data1/LQAnalyzer_rootfiles_for_analysis/DataSetLists/"
-export LQANALYZER_SKTreeLOG_DIR="/data1/LQAnalyzer_rootfiles_for_analysis/CATSKTreeMaker/"
-export CATTAGDIR="/data1/LQAnalyzer_rootfiles_for_analysis/CATTag/"
+export ANALYZER_FILE_DIR="/data1/${Flag}Analyzer_rootfiles_for_analysis/CATAnalysis2016/"
+export ANALYZER_DATASETFILE_DIR="/data1/${Flag}Analyzer_rootfiles_for_analysis/DataSetLists/AnalysisFiles/"
+export ANALYZER_DATASET_DIR="/data1/${Flag}Analyzer_rootfiles_for_analysis/DataSetLists/"
+export ANALYZER_SKTreeLOG_DIR="/data1/${Flag}Analyzer_rootfiles_for_analysis/CATSKTreeMaker/"
+export CATTAGDIR="/data1/${Flag}Analyzer_rootfiles_for_analysis/CATTag/"
 if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
 then
-    export LQANALYZER_FILE_DIR="/data2/LQAnalyzer_rootfiles_for_analysis/CATAnalysis2016/"
-    export LQANALYZER_DATASETFILE_DIR="/data2/LQAnalyzer_rootfiles_for_analysis/DataSetLists/AnalysisFiles/"
-    export LQANALYZER_DATASET_DIR="/data2/LQAnalyzer_rootfiles_for_analysis/DataSetLists/"
-    export LQANALYZER_SKTreeLOG_DIR="/data2/LQAnalyzer_rootfiles_for_analysis/CATSKTreeMaker/"
-    export CATTAGDIR="/data2/LQAnalyzer_rootfiles_for_analysis/CATTag/"
+    export ANALYZER_FILE_DIR="/data2/${Flag}Analyzer_rootfiles_for_analysis/CATAnalysis2016/"
+    export ANALYZER_DATASETFILE_DIR="/data2/${Flag}Analyzer_rootfiles_for_analysis/DataSetLists/AnalysisFiles/"
+    export ANALYZER_DATASET_DIR="/data2/${Flag}Analyzer_rootfiles_for_analysis/DataSetLists/"
+    export ANALYZER_SKTreeLOG_DIR="/data2/${Flag}Analyzer_rootfiles_for_analysis/CATSKTreeMaker/"
+    export CATTAGDIR="/data2/${Flag}Analyzer_rootfiles_for_analysis/CATTag/"
 fi
 
 export running2015=False
 
 # Modify to describe your directory structure.
-# all directories are below the LQAnalyser base directory specified above
+# all directories are below the ${Flag}Analyser base directory specified above
 ### setup paths to be used in analysis code
-export LQANALYZER_ANALYSIS_PATH=${LQANALYZER_DIR}/LQAnalysis/
-export LQANALYZER_SRC_PATH=${LQANALYZER_DIR}/LQAnalysis/Analyzers/src/
-export LQANALYZER_INCLUDE_PATH=${LQANALYZER_DIR}/LQAnalysis/Analyzersinclude/
-export LQANALYZER_CORE_PATH=${LQANALYZER_DIR}/LQCore/
+export ANALYZER_ANALYSIS_PATH=${ANALYZER_DIR}/${Flag}Analysis/
+export ANALYZER_SRC_PATH=${ANALYZER_DIR}/${Flag}Analysis/Analyzers/src/
+export ANALYZER_INCLUDE_PATH=${ANALYZER_DIR}/${Flag}Analysis/Analyzers/include/
+export ANALYZER_CORE_PATH=${ANALYZER_DIR}/${Flag}Core/
 
 export isSLC5="False"
-export BTAGDIR=${LQANALYZER_DIR}/LQAnalysis/AnalyzerTools/BTag/BTagC11/
-export ROCHDIR=${LQANALYZER_DIR}/LQAnalysis/AnalyzerTools/rochcor2016/
+export BTAGDIR=${ANALYZER_DIR}/${Flag}Analysis/AnalyzerTools/BTag/BTagC11/
+export ROCHDIR=${ANALYZER_DIR}/${Flag}Analysis/AnalyzerTools/rochcor2016/
 if [[ "$HOSTNAME" == "cms.snu.ac.kr" ]];
 then 
     if [[ $LIBTAG == *"v"* ]]; then
 	export OBJ=obj/cms21$LIBTAG
-	export LQANALYZER_LIB_PATH=${LQANALYZER_DIR}/LQLib/cms21$LIBTAG/
-        export LQANALYZER_BATCHLIB_PATH=${LQANALYZER_DIR}/LQLib/batch/
+	export ANALYZER_LIB_PATH=${ANALYZER_DIR}/${Flag}Lib/cms21$LIBTAG/
+        export ANALYZER_BATCHLIB_PATH=${ANALYZER_DIR}/${Flag}Lib/batch/
 	
     else 
 	export OBJ=obj/cms21
-        export LQANALYZER_LIB_PATH=${LQANALYZER_DIR}/LQLib/cms21/
-	export LQANALYZER_BATCHLIB_PATH=${LQANALYZER_DIR}/LQLib/batch/
+        export ANALYZER_LIB_PATH=${ANALYZER_DIR}/${Flag}Lib/cms21/
+	export ANALYZER_BATCHLIB_PATH=${ANALYZER_DIR}/${Flag}Lib/batch/
     fi
 elif [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
 then
     export OBJ=obj/cluster/
-    export LQANALYZER_LIB_PATH=${LQANALYZER_DIR}/LQLib/cluster/
+    export ANALYZER_LIB_PATH=${ANALYZER_DIR}/${Flag}Lib/cluster/
 
 elif [[ "$HOSTNAME" == "cms1" ]];
 then
     export OBJ=obj/cms1
-    export LQANALYZER_LIB_PATH=${LQANALYZER_DIR}/LQLib/cms1/
+    export ANALYZER_LIB_PATH=${ANALYZER_DIR}/${Flag}Lib/cms1/
 
 else
     export OBJ=obj/cms2
-    export LQANALYZER_LIB_PATH=${LQANALYZER_DIR}/LQLib/cms2/
+    export ANALYZER_LIB_PATH=${ANALYZER_DIR}/${Flag}Lib/cms2/
 
 fi
 
-export LQANALYZER_OLDLIB_PATH=${LQANALYZER_DIR}/LQLib/
+export ANALYZER_OLDLIB_PATH=${ANALYZER_DIR}/${Flag}Lib/
 
-export LQANALYZER_RUN_PATH=${LQANALYZER_DIR}/LQRun/
-export LQANALYZER_CLUSTER_TXT_PATH=${LQANALYZER_DIR}/LQRun/txt/Cluster/
-export LQANALYZER_BIN_PATH=${LQANALYZER_DIR}/bin/
+export ANALYZER_RUN_PATH=${ANALYZER_DIR}/${Flag}Run/
+export ANALYZER_CLUSTER_TXT_PATH=${ANALYZER_DIR}/${Flag}Run/txt/Cluster/
+export ANALYZER_BIN_PATH=${ANALYZER_DIR}/bin/
 ### set SKTree path
-export SKTREE_INCLUDE_PATH=${LQANALYZER_DIR}/LQCore/SKTree/include/
+export SKTREE_INCLUDE_PATH=${ANALYZER_DIR}/${Flag}Core/SKTree/include/
 ## setup directory to store analysis rootfiles
-export FILEDIR=${LQANALYZER_DIR}/data/rootfiles/
-export IDFILEDIR=${LQANALYZER_DIR}/data/ID/80X/
-export LUMIFILEDIR=${LQANALYZER_DIR}/data/Luminosity/80X/
-export TRIGGERFILEDIR=${LQANALYZER_DIR}/data/Trigger/80X/
-export BTAGFILEDIR=${LQANALYZER_DIR}/data/BTag/80X/
-export PILEUPFILEDIR=${LQANALYZER_DIR}/data/Pileup/80X/
+export FILEDIR=${ANALYZER_DIR}/data/rootfiles/
+export IDFILEDIR=${ANALYZER_DIR}/data/ID/80X/
+export LUMIFILEDIR=${ANALYZER_DIR}/data/Luminosity/80X/
+export TRIGGERFILEDIR=${ANALYZER_DIR}/data/Trigger/80X/
+export BTAGFILEDIR=${ANALYZER_DIR}/data/BTag/80X/
+export PILEUPFILEDIR=${ANALYZER_DIR}/data/Pileup/80X/
 
 
 
-if [ ! -d ${LQANALYZER_OLDLIB_PATH} ]; then
-    echo Directory ${LQANALYZER_OLDLIB_PATH} does not exist ... creating it
-    mkdir ${LQANALYZER_OLDLIB_PATH}
+if [ ! -d ${ANALYZER_OLDLIB_PATH} ]; then
+    echo Directory ${ANALYZER_OLDLIB_PATH} does not exist ... creating it
+    mkdir ${ANALYZER_OLDLIB_PATH}
 fi
 
-if [ ! -d ${LQANALYZER_LIB_PATH} ]; then
-    echo Directory ${LQANALYZER_LIB_PATH} does not exist ... creating it
-    mkdir ${LQANALYZER_LIB_PATH}
-    file="${LQANALYZER_OLDLIB_PATH}/libAnalysisCore.so"
+if [ ! -d ${ANALYZER_LIB_PATH} ]; then
+    echo Directory ${ANALYZER_LIB_PATH} does not exist ... creating it
+    mkdir ${ANALYZER_LIB_PATH}
+    file="${ANALYZER_OLDLIB_PATH}/libAnalysisCore.so"
     if [ -f "$file" ]; then
-	echo Old lib dir ${LQANALYZER_OLDLIB_PATH} is redundant. Will remove these library
-	rm  ${LQANALYZER_OLDLIB_PATH}/*.so
-	rm  ${LQANALYZER_OLDLIB_PATH}/*map
-	rm  ${LQANALYZER_CORE_PATH}/*/obj/*.o
-	rm -r ${LQANALYZER_CORE_PATH}/*/obj/dep/
-	rm  ${LQANALYZER_ANALYSIS_PATH}/*/obj/*.o
-	rm -r ${LQANALYZER_ANALYSIS_PATH}/*/obj/dep/
+	echo Old lib dir ${ANALYZER_OLDLIB_PATH} is redundant. Will remove these library
+	rm  ${ANALYZER_OLDLIB_PATH}/*.so
+	rm  ${ANALYZER_OLDLIB_PATH}/*map
+	rm  ${ANALYZER_CORE_PATH}/*/obj/*.o
+	rm -r ${ANALYZER_CORE_PATH}/*/obj/dep/
+	rm  ${ANALYZER_ANALYSIS_PATH}/*/obj/*.o
+	rm -r ${ANALYZER_ANALYSIS_PATH}/*/obj/dep/
     fi
 fi
 
 ### Load useful functions
-source ${LQANALYZER_BIN_PATH}/cleanup.sh 
+source ${ANALYZER_BIN_PATH}/cleanup.sh 
 ### make directories that git does not allow to store
 
-export LQANALYZER_OUTPUT_PATH=/data2/CAT_SKTreeOutput/JobOutPut/${USER}/LQanalyzer/data/output/
+export ANALYZER_OUTPUT_PATH=/data2/CAT_SKTreeOutput/JobOutPut/${USER}/${Flag}analyzer/data/output/
 
-export LQANALYZER_LOG_PATH=/data2/CAT_SKTreeOutput/JobOutPut/${USER}/LQanalyzer/data/logfiles/
-export LQANALYZER_LOG_8TeV_PATH=${LQANALYZER_DIR}/data/logfiles/
+export ANALYZER_LOG_PATH=/data2/CAT_SKTreeOutput/JobOutPut/${USER}/${Flag}analyzer/data/logfiles/
+export ANALYZER_LOG_8TeV_PATH=${ANALYZER_DIR}/data/logfiles/
 
 if [ $HOSTNAME == "tamsa2.snu.ac.kr" ];
 then
-    export LQANALYZER_OUTPUT_PATH=/data4/CAT_SKTreeOutput/JobOutPut/${USER}/LQanalyzer/data/output/
-    export LQANALYZER_LOG_PATH=/data4/CAT_SKTreeOutput/JobOutPut/${USER}/LQanalyzer/data/logfiles/
+    export ANALYZER_OUTPUT_PATH=/data4/CAT_SKTreeOutput/JobOutPut/${USER}/${Flag}analyzer/data/output/
+    export ANALYZER_LOG_PATH=/data4/CAT_SKTreeOutput/JobOutPut/${USER}/${Flag}analyzer/data/logfiles/
 fi
 
-python ${LQANALYZER_DIR}/python/SetUpWorkSpace.py
-python ${LQANALYZER_DIR}/python/BackUpDirectory.py
-python ${LQANALYZER_DIR}/python/SetupEmailList.py
+python ${ANALYZER_DIR}/python/SetUpWorkSpace.py
+python ${ANALYZER_DIR}/python/BackUpDirectory.py
+python ${ANALYZER_DIR}/python/SetupEmailList.py
 
 # Setup root area and other paths
  
@@ -370,7 +378,7 @@ if [[ `root-config --platform` == "macosx" ]]; then
 
     # With Fink ROOT installations, DYLD_LIBRARY_PATH doesn't have
     # to be defined for ROOT to work. So let's leave the test for it...
-    export DYLD_LIBRARY_PATH=${LQANALYZER_LIB_PATH}:${DYLD_LIBRARY_PATH}
+    export DYLD_LIBRARY_PATH=${ANALYZER_LIB_PATH}:${DYLD_LIBRARY_PATH}
     
 else    
     
@@ -378,20 +386,20 @@ else
         echo "Warning: so far you haven't setup your ROOT enviroment properly (no LD_LIBRARY_PATH): FrameWork will not work"
     fi
     export LD_TMP_LIBRARY_PATH=${LD_LIBRARY_PATH}
-    export LD_LIBRARY_PATH=${LQANALYZER_LIB_PATH}:${LD_LIBRARY_PATH}
+    export LD_LIBRARY_PATH=${ANALYZER_LIB_PATH}:${LD_LIBRARY_PATH}
 
 fi
 
 
-export PATH=${LQANALYZER_BIN_PATH}:${PATH}
-export PYTHONPATH=${LQANALYZER_DIR}/python:${PYTHONPATH}
-export PAR_PATH=./:${LQANALYZER_LIB_PATH}
+export PATH=${ANALYZER_BIN_PATH}:${PATH}
+export PYTHONPATH=${ANALYZER_DIR}/python:${PYTHONPATH}
+export PAR_PATH=./:${ANALYZER_LIB_PATH}
 
-python ${LQANALYZER_DIR}/python/local_check.py
+python ${ANALYZER_DIR}/python/local_check.py
 
-if [ ! -d ${LQANALYZER_LOG_PATH} ]; then
-    echo Directory ${LQANALYZER_LOG_PATH} does not exist ... creating it
-    mkdir ${LQANALYZER_LOG_PATH}
+if [ ! -d ${ANALYZER_LOG_PATH} ]; then
+    echo Directory ${ANALYZER_LOG_PATH} does not exist ... creating it
+    mkdir ${ANALYZER_LOG_PATH}
 fi
 
 echo "Running analysis from" $HOSTNAME " in directory: " $PWD
